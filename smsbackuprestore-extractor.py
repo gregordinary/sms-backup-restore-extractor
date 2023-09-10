268b100bf036e1a0271f038fdac60728f36f1113
gregordinary <33880035+gregordinary@users.noreply.github.com>
2023-09-10 01:17:58 -0400

    Update smsbackuprestore-extractor.py
    Add thread locks
    Add logfile
    Add optional console-output flag --log-to-console

--- smsbackuprestore-extractor.py
+++ smsbackuprestore-extractor.py
@@ -53,11 +53,11 @@ except ModuleNotFoundError:
     print("You can install it by running 'pip install prettytable' in your command line.")
     sys.exit(1)
 
-
 # Remaining Standard Library Imports
 import argparse
 import base64
 import datetime
+import errno 
 import hashlib
 import logging
 import pickle
@@ -67,35 +67,96 @@ import traceback
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
+# Logging Configuration
+log_filename = "xml-extract.log"
+
 # Define the is_windows flag
 is_windows = sys.platform == 'win32'
 
-def process_mms(mms, output_folder, saved_hashes, saved_hashes_file, write_hash_on):
-    global total_duplicate_images_skipped
+# Define the GlobalStats class
+class GlobalStats:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.total_folders_created = 0
+        self.total_files_created = 0
+        self.total_duplicate_images_skipped = 0
+        self.total_errors = 0
+
+    def increment_folders_created(self):
+        with self.lock:
+            self.total_folders_created += 1
+
+    def increment_files_created(self):
+        with self.lock:
+            self.total_files_created += 1
+
+    def increment_duplicate_images_skipped(self):
+        with self.lock:
+            self.total_duplicate_images_skipped += 1
+
+    def increment_errors(self):
+        with self.lock:
+            self.total_errors += 1
+
+def initialize_logging(log_to_console):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # create a file handler
+    handler = logging.FileHandler(log_filename)
+    handler.setLevel(logging.INFO)
+
+    # create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # Create a console handler with a higher log level
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+    console_handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler)
+    logger.addHandler(console_handler)
+
+    if log_to_console:
+        # create a stream handler (console)
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setLevel(logging.INFO)
+        consoleHandler.setFormatter(formatter)
+        # add the handler to the logger
+        logger.addHandler(consoleHandler)
+
+
+def process_mms(mms, output_folder, saved_hashes, saved_hashes_file, write_hash_on, global_stats, lock):
     media_list = get_media_list(mms)
     folder = get_folder_name(mms)
-    folder_hashes = saved_hashes.get(folder, set())
-    output = get_output_folder(output_folder, folder)
+    with lock:
+        folder_hashes = saved_hashes.get(folder, set())
+    output = get_output_folder(output_folder, folder, global_stats)
 
     for media in media_list:
         rawdata, sha256, filename = get_file_data(media)
 
         if sha256 in folder_hashes:
             logging.info("Duplicate file skipped: %s", filename)
-            total_duplicate_images_skipped += 1
+            global_stats.increment_duplicate_images_skipped()
             continue
 
         outfile = os.path.join(output, filename)
         timestamp = datetime.datetime.fromtimestamp(float(mms.get("date")) / 1000.0)
 
-        write_file(outfile, rawdata, timestamp, is_windows)
-        folder_hashes.add(sha256)
+        write_file(outfile, rawdata, timestamp, is_windows, global_stats)
+        with lock:
+           folder_hashes.add(sha256)
         
         if write_hash_on == 'media':
-            update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file)
+            with lock:
+                update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file, global_stats)
     
     if write_hash_on == 'mms':
-        update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file)
+        with lock:
+            update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file, global_stats)
         
     mms.clear()
 
@@ -112,13 +173,12 @@ def get_folder_name(mms):
         folder = contact
     return folder
 
-def get_output_folder(output_folder, folder):
-    global total_folders_created
+def get_output_folder(output_folder, folder, global_stats):
     output = os.path.join(output_folder, folder)
     if not os.path.exists(output):
         os.makedirs(output)
         logging.info("New folder created: %s", folder)
-        total_folders_created += 1
+        global_stats.increment_folders_created()
     return output
 
 def get_file_data(media):
@@ -147,17 +207,19 @@ def get_file_data(media):
     return rawdata, sha256, filename
 
 
-def write_file(outfile, rawdata, timestamp, is_windows):
-    global total_files_created
-    global total_errors
-    
+def write_file(outfile, rawdata, timestamp, is_windows, global_stats):
     try:
         with open(outfile, 'wb') as f:
             f.write(rawdata)
-        total_files_created += 1
+        global_stats.increment_files_created()
     except IOError as e:
-        logging.error("Unable to write to file %s: %s", outfile, e)
-        total_errors += 1
+        if e.errno == errno.ENOSPC:  # if the error is "No space left on device"
+            logging.error("No space left on the output device.")
+        elif e.errno == errno.EACCES:  # if the error is "Permission denied"
+            logging.error("You don't have permission to create this file.")
+        else:
+            logging.error("Unknown error occurred while writing the file: %s", str(e))
+        global_stats.increment_errors()
         return
 
     if os.path.exists(outfile):
@@ -170,22 +232,18 @@ def write_file(outfile, rawdata, timestamp, is_windows):
                 os.utime(outfile, ns=(int(filetime_ns), int(filetime_ns)))
         except Exception as e:
             logging.error("Unable to set the file time for %s: %s", outfile, e)
-            total_errors += 1
+            global_stats.increment_errors()
 
-def update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file):
+def update_saved_hashes(saved_hashes, folder, folder_hashes, saved_hashes_file, global_stats):
     saved_hashes[folder] = folder_hashes
     try:
         with open(saved_hashes_file, 'wb') as f:
             pickle.dump(saved_hashes, f)
     except IOError as e:
         logging.error("Unable to write saved hashes file: %s", e)
-        total_errors += 1
+        global_stats.increment_errors()
 
-
-def initialize_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-def load_saved_hashes(saved_hashes_file):
+def load_saved_hashes(saved_hashes_file, global_stats):
     try:
         with open(saved_hashes_file, 'rb') as f:
             saved_hashes = pickle.load(f)
@@ -193,23 +251,24 @@ def load_saved_hashes(saved_hashes_file):
         saved_hashes = {}
     except IOError as e:
         logging.error("Unable to read saved hashes file: %s", e)
-        total_errors += 1
+        global_stats.increment_errors()
         sys.exit(1)
     return saved_hashes
 
-def process_xml_files(input_path, output_folder, num_threads, saved_hashes, saved_hashes_file, max_depth, huge_tree, write_hash_on):
+def process_xml_files(input_path, output_folder, num_threads, saved_hashes, saved_hashes_file, max_depth, huge_tree, write_hash_on, global_stats):
+    lock = threading.Lock()
     futures = []
     def process_xml_file(input_file):
         logging.info("Parsing: %s", input_file)
         with ThreadPoolExecutor(max_workers=num_threads) as executor:
             for _, mms in etree.iterparse(input_file, tag='mms', huge_tree=huge_tree):
-                futures.append(executor.submit(process_mms, mms, output_folder, saved_hashes, saved_hashes_file, write_hash_on))
+                futures.append(executor.submit(process_mms, mms, output_folder, saved_hashes, saved_hashes_file, write_hash_on, global_stats, lock))
             for future in as_completed(futures):
                 try:
                     future.result()
-                except Exception as e:
-                    logging.error("Exception: %s", e)
-                    total_errors += 1
+                except lxml.etree.XMLSyntaxError as e:
+                    logging.error("XML syntax error occurred while parsing the file: %s", str(e))
+                    global_stats.increment_errors()
         if write_hash_on == 'xml':
             update_saved_hashes(saved_hashes, None, None, saved_hashes_file)
 
@@ -245,33 +304,21 @@ def format_timedelta(td):
     else:
         return f'{milliseconds}ms'
 
-def main(input_path, output_folder, num_threads, saved_hashes_file, max_depth, huge_tree, write_hash_on):
-    global total_files_created
-    global total_size_created
-    global total_duplicate_images_skipped
-    global total_errors
-    initialize_logging()
+def main(input_path, output_folder, num_threads, saved_hashes_file, max_depth, huge_tree, write_hash_on, log_to_console):
+    global_stats = GlobalStats()
+    initialize_logging(log_to_console)
     try:
-        saved_hashes = load_saved_hashes(saved_hashes_file)
+        saved_hashes = load_saved_hashes(saved_hashes_file, global_stats)
     except IOError as e:
         logging.error("Unable to read saved hashes file: %s", e)
-        total_errors += 1
+        global_stats.increment_errors()
         sys.exit(1)
 
     try:
-        process_xml_files(input_path, output_folder, num_threads, saved_hashes, saved_hashes_file, max_depth, huge_tree, write_hash_on)
+        process_xml_files(input_path, output_folder, num_threads, saved_hashes, saved_hashes_file, max_depth, huge_tree, write_hash_on, global_stats)
     except Exception as e:
         logging.error("Exception: %s", e)
-        total_errors += 1
-
-    # # display summary
-    # logging.info("Job done")
-    # logging.info("Output folder: %s", output_folder)
-    # logging.info("Total run time: %s", str(datetime.timedelta(seconds=(time.time() - start_time))))
-    # logging.info("Total folders created: %d", total_folders_created)
-    # logging.info("Total files created: %d", total_files_created)
-    # logging.info("Total duplicate images skipped: %d", total_duplicate_images_skipped)
-    # logging.info("Total errors: %d", total_errors)
+        global_stats.increment_errors()
 
     # display summary
     table = PrettyTable()
@@ -279,18 +326,14 @@ def main(input_path, output_folder, num_threads, saved_hashes_file, max_depth, h
     table.align["Metric"] = "l"
     table.align["Value"] = "c"
     table.add_row(["Run Time", format_timedelta(datetime.timedelta(seconds=(time.time() - start_time)))])
-    table.add_row(["Folders Created", total_folders_created])
-    table.add_row(["Files Created", total_files_created])
-    table.add_row(["Duplicate Images Skipped", total_duplicate_images_skipped])
-    table.add_row(["Total Errors", total_errors])
+    table.add_row(["Folders Created", global_stats.total_folders_created])
+    table.add_row(["Files Created", global_stats.total_files_created])
+    table.add_row(["Duplicate Images Skipped", global_stats.total_duplicate_images_skipped])
+    table.add_row(["Total Errors", global_stats.total_errors])
     print(table)
 
 if __name__ == "__main__":
     start_time = time.time()
-    total_folders_created = 0
-    total_files_created = 0
-    total_duplicate_images_skipped = 0
-    total_errors = 0
 
     parser = argparse.ArgumentParser(description='SMSBackupRestore extractor')
     parser.add_argument('input_path', type=str,
@@ -308,7 +351,8 @@ if __name__ == "__main__":
     parser.add_argument('--write-hash-on', type=str, default='media',
                         choices=['media', 'mms', 'xml', 'run'],
                         help='When to update the saved_hashes file (default: media)')
-
+    parser.add_argument('--log-to-console', action='store_true',
+                    help='Log to console in addition to the log file')
 
     args = parser.parse_args()
 
@@ -317,4 +361,4 @@ if __name__ == "__main__":
     else:
         saved_hashes_file = args.saved_hashes
 
-    main(args.input_path, args.output_folder, args.threads, saved_hashes_file, args.max_depth, args.huge_tree, args.write_hash_on)
+    main(args.input_path, args.output_folder, args.threads, saved_hashes_file, args.max_depth, args.huge_tree, args.write_hash_on, args.log_to_console)
